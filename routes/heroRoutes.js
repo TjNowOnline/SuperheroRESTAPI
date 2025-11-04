1ab97179e45030ff7fe9390f1997b2c30239e636
theisjensen <theis1@live.dk>
2025-11-04 11:47:42 +0100

    FIX

--- routes/heroRoutes.js
+++ routes/heroRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const heroController = require("../controllers/heroController");
-const { validateHero } = require("../middlewares/validateHero");
+const { validateHero } = require("../validators/favoriteValidator");
 
 router.get("/superheroes", heroController.getAllHeroes);
 router.get("/superheroes/:id", heroController.getHeroById);
 
-router.post("/favorite", validateHero, heroController.addFavoriteHero);
-router.delete("/favorite/:id", heroController.removeFavoriteHero);
-router.get("/favorites", heroController.getFavoriteHeroes);
+router.post("/favorite", validateHero, heroController.addFavorite);
+router.delete("/favorite/:id", heroController.deleteFavorite);
+router.get("/favorites", heroController.getFavorites);
 
 module.exports = router;
